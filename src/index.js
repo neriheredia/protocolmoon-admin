bee1e12326ee01d2b380c48d48dd7e5ce25eb7e5
neriheredia <developer.heredia@gmail.com>
2022-04-01 16:20:48 -0300

    Redux Instalado

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { DarkModeProvider } from "./context/darkModeContext";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <DarkModeProvider>
-      <App />
-    </DarkModeProvider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <DarkModeProvider>
+        <App />
+      </DarkModeProvider>
+    </PersistGate>
+  </Provider>,
   document.getElementById("root")
 );
